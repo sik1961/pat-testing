817e7abd39f0cda78625e49f6f86009cda69f352
Si Kusners <simon.kusners@gmail.com>
2017-12-14 11:11:13 +0000

    Change test results to CRUD repo

--- src/main/java/com/sik/pat/repository/TestResultRepository.java
+++ src/main/java/com/sik/pat/repository/TestResultRepository.java
@@ -1,11 +1,19 @@
 package com.sik.pat.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
+import com.sik.pat.domain.Customer;
 import com.sik.pat.domain.TestResult;
 
 @Component
-public interface TestResultRepository extends JpaRepository<TestResult, String>{
+public interface TestResultRepository extends CrudRepository<TestResult, String>{
+	
+	@Query("select r from TestResult r where r.cust_id = ':cust_id'")
+    Set<Customer> findResultsByCustomer(@Param(value="cust_id") String customerId);
 
 }
